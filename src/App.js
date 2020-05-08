e11e35aedee0a06df2aaf89af7e91580b2746444
Jen <jentrinanes@hotmail.com>
2020-05-08 19:29:31 +0800

    Modified Addp.js and added some blank pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,141 @@
-import React from 'react';
+import PropTypes from "prop-types";
+import React, { useEffect, Suspense, lazy } from "react";
+import ScrollToTop from "./helpers/scroll-top";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ToastProvider } from "react-toast-notifications";
+import { multilanguage, loadLanguages } from "redux-multilanguage";
+import { connect } from "react-redux";
+import { BreadcrumbsProvider } from "react-breadcrumbs-dynamic";
 
-function App() {
+// home page
+const HomeCosmetics = lazy(() => import("./pages/home/HomeCosmetics"));
+
+// shop page
+const ShopGridStandard = lazy(() => import("./pages/shop/ShopGridStandard"));
+
+// product page
+const ProductTabLeft = lazy(() =>
+  import("./pages/shop-product/ProductTabLeft")
+);
+
+// blog page
+const BlogRightSidebar = lazy(() => import("./pages/blog/BlogRightSidebar"));
+
+// other pages
+const About = lazy(() => import("./pages/other/About"));
+const Contact = lazy(() => import("./pages/other/Contact"));
+const MyAccount = lazy(() => import("./pages/other/MyAccount"));
+const LoginRegister = lazy(() => import("./pages/other/LoginRegister"));
+
+const Cart = lazy(() => import("./pages/other/Cart"));
+const Wishlist = lazy(() => import("./pages/other/Wishlist"));
+const Compare = lazy(() => import("./pages/other/Compare"));
+const Checkout = lazy(() => import("./pages/other/Checkout"));
+
+const NotFound = lazy(() => import("./pages/other/NotFound"));
+
+const App = props => {
+  useEffect(() => {
+    props.dispatch(
+      loadLanguages({
+        languages: {
+          en: require("./translations/english.json")
+        }
+      })
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="#" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <ToastProvider placement="bottom-left">
+      <BreadcrumbsProvider>
+        <Router>
+          <ScrollToTop>
+            <Suspense
+              fallback={
+                <div className="flone-preloader-wrapper">
+                  <div className="flone-preloader">
+                    <span></span>
+                    <span></span>
+                  </div>
+                </div>
+              }
+            >
+              <Switch>                
+                <Route
+                  path={process.env.PUBLIC_URL + "/home-cosmetics"}
+                  component={HomeCosmetics}
+                />                
 
-export default App;
+                {/* Shop page */}
+                <Route
+                  path={process.env.PUBLIC_URL + "/shop-grid-standard"}
+                  component={ShopGridStandard}
+                />                
+
+                {/* Shop product page */}                
+                <Route
+                  path={process.env.PUBLIC_URL + "/product-tab-left/:id"}
+                  component={ProductTabLeft}
+                />                
+
+                {/* Blog page */}               
+                <Route
+                  path={process.env.PUBLIC_URL + "/blog-right-sidebar"}
+                  component={BlogRightSidebar}
+                />
+               
+                {/* Other pages */}
+                <Route
+                  path={process.env.PUBLIC_URL + "/about"}
+                  component={About}
+                />
+                <Route
+                  path={process.env.PUBLIC_URL + "/contact"}
+                  component={Contact}
+                />
+                <Route
+                  path={process.env.PUBLIC_URL + "/my-account"}
+                  component={MyAccount}
+                />
+                <Route
+                  path={process.env.PUBLIC_URL + "/login-register"}
+                  component={LoginRegister}
+                />
+
+                <Route
+                  path={process.env.PUBLIC_URL + "/cart"}
+                  component={Cart}
+                />
+                <Route
+                  path={process.env.PUBLIC_URL + "/wishlist"}
+                  component={Wishlist}
+                />
+                <Route
+                  path={process.env.PUBLIC_URL + "/compare"}
+                  component={Compare}
+                />
+                <Route
+                  path={process.env.PUBLIC_URL + "/checkout"}
+                  component={Checkout}
+                />
+
+                <Route
+                  path={process.env.PUBLIC_URL + "/not-found"}
+                  component={NotFound}
+                />
+
+                <Route exact component={NotFound} />
+              </Switch>
+            </Suspense>
+          </ScrollToTop>
+        </Router>
+      </BreadcrumbsProvider>
+    </ToastProvider>
+  );
+};
+
+App.propTypes = {
+  dispatch: PropTypes.func
+};
+
+export default connect()(multilanguage(App));
