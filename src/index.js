9dd75ace91bffa9ec59c90fdebf8ea16d250f205
Jen <jentrinanes@hotmail.com>
2020-05-08 16:54:00 +0800

    Modified index.js and added some css files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { save, load } from 'redux-localstorage-simple';
+import { Provider } from 'react-redux';
+import { fetchProducts } from './redux/actions/productActions';
+import rootReducer from './redux/reducers/rootReducer';
+import products from './data/products.json';
 import App from './App';
+import './assets/scss/style.scss';
 import * as serviceWorker from './serviceWorker';
 
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(
+  rootReducer,
+  load(),
+  composeWithDevTools(applyMiddleware(thunk, save()))
+);
+
+// fetch products from json file
+store.dispatch(fetchProducts(products));
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
